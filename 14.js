5821a9005c713dc6b7021b484d91959b8868c779
nabilaafifahqotrunnada <nabila_afifah_28rpl@student.smktelkom-mlg.sch.id>
2020-07-25 23:35:09 +0700

    Second Commit

---
+++ 14.js
@@ -0,0 +1,12 @@
+//For Loop
+
+/*statement index pertama dieksekusi sebelum proses (berisi nilai awal),
+statement kedua akan membandingkan index dengan bilangan numerik lain
+bertujuan mengukur banyaknya perulangan yang akan dieksekusi, 
+statement ketiga bertujuan untuk eksekusi setiap kali 
+statement kedua bernilai true serta terjadinya perubahan nilai index
+hingga statement berni=lai false*/
+for(let index = 1; index <= 10; index++) {
+    console.log("Perulangan ke-" + index);
+}
+//terjadi 10 looping karena kondisi akan bernilai false saat index > 10
